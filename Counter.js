795115133a46609078676f507bde5ef9ff525893
peanut-lover <jihoson94@gmail.com>
2023-02-17 13:01:11 +0900

    Stale State

--- Counter.js
+++ Counter.js
@@ -1,21 +1,19 @@
+// Stale Cloure
 function useState(initialValue) {
   var _val = initialValue; // _val is a local variable created by useState
-  function state() {
-    // state is an inner function, a closure
-    return _val; // state() uses _val, declared by parent funciton
-  }
+
   function setState(newVal) {
     // same
     _val = newVal; // setting _val without exposing _val
   }
-  return [state, setState]; // exposing functions for external use
+  return [_val, setState]; // exposing functions for external use
 }
 
 function Counter() {
   const [count, setCount] = useState(0);
   return {
-    click: () => setCount(count() + 1),
-    render: () => console.log("render: ", { count: count() }),
+    click: () => setCount(count + 1),
+    render: () => console.log("render: ", { count }),
   };
 }
 
